e59bb39a9142bf1bed423bb2b6ee3aafb0528eef
Avery.Davenport <averymdavenport@gmail.com>
2021-06-17 16:33:35 -0600

    2-commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,17 +4,14 @@ const bcrypt = require('bcryptjs');
 module.exports = {
       login: (req, res) => {
       console.log('Logging In User');
-
       const { username, password } = req.body;
-
       for (let i = 0; i < users.length; i++) {
         if (users[i].username === username) {
         const existing = bcrypt.compareSync(password, users[i].passHash)
         if (existing) {
             let userToReturn = {...users[i]}
             delete userToReturn.passHash
-          return res.status(200).send(userToReturn);
-          
+          return res.status(200).send(userToReturn); 
         }
       }
     }
@@ -22,20 +19,15 @@ module.exports = {
   },
 
     register: (req, res) => {
-
         const {username, email, firstName, lastName, password} = req.body;
-
         for (i=0; i < users.length; i++){
           const existing = bcrypt.compareSync(password, users[i].passHash);
-
           if (existing) {
             return
-          }
+           }
         };
-
         const salt = bcrypt.genSaltSync(5);
         const passHash = bcrypt.hashSync(password, salt);
-
         let userObj = {
           passHash,
           username,
@@ -43,13 +35,10 @@ module.exports = {
           firstName,
           lastName,
         }
-
         console.log('Registering User');
         users.push(userObj);
-        // console.log(users);
         let infoToReturn = {...userObj};
         delete infoToReturn.passHash;
         res.status(200).send(infoToReturn);
     }
 }
-// console.log(users)
